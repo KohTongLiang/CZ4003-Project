a2ea10620494dd7bb06758d32d7a6ec69628b268
KohTongLiang <koh.tongliang@gmail.com>
2021-11-19 23:06:04 +0800

    For submission

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ import math
 debug_dir = './debug'
 im_dir = './images'
 
+# otsu global thresholding implementation
 def otsu_t (img):
     print('Calculating Otsu threshold')
     ## de-noising
     # add gaussian blur to reduce image noise
-    # img = cv2.GaussianBlur(img, (5,5),0)
+    img = cv2.GaussianBlur(img, (5,5),0)
 
     cv2.imwrite(f'{debug_dir}/gblur_{args.imname}.jpg', img) # checkpoint 1 gaussian blur
-
-    ## threshold calculation
 
     # get image histogram
     bins = 256
     hist, bin_edges = np.histogram(img, bins=bins)
 
-    # get histogram plot
+    # save histogram plot for analysis later
     pl.hist(hist)
     pl.title(f'{args.imname}')
     pl.xlabel("value")
@@ -39,10 +38,8 @@ def otsu_t (img):
     w1 = np.cumsum(hist)
     w2 = np.cumsum(hist[::-1])[::-1]
 
-    # get mean of class mu0(t)
+    # get mean of class u0(t) and u1(t)
     mu0 = np.cumsum(hist * bin_mids) / w1
-
-    # get mean of class mu1(t)
     mu1 = (np.cumsum((hist * bin_mids)[::-1]) / w2[::-1])[::-1]
 
     # get inter class variance
@@ -51,6 +48,7 @@ def otsu_t (img):
     # maximise inter class variance
     i = np.argmax(inter_class_variance)
 
+    # get threshold value
     th = bin_mids[:-1][i]
 
     print(f'Threshold calculated: {th}')
@@ -60,105 +58,104 @@ def otsu_t (img):
 
     return output
 
-def improvement1(img):
-    output = img
-    output1 = cv2.medianBlur(output,3) # blur 1
-    output2 = cv2.medianBlur(output,51) # blur 2
+# Using edge detectors
+# takes in an additional flag parameter to 
+def improvement1 (img, flag=1):
+    if flag == 1:
+        # create sobel mask
+        Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+        Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+        [rows, columns] = np.shape(img)  # we need to know the shape of the input grayscale image
+        output = np.zeros(shape=(rows, columns))  # initialization of the output image array (all elements are 0)
 
-    # divide the 2 blur images and normalize the pixel values
+        # Compute output in x and y directions
+        for i in range(rows - 2):
+            for j in range(columns - 2):
+                gx = np.sum(np.multiply(Gx, img[i:i + 3, j:j + 3]))
+                gy = np.sum(np.multiply(Gy, img[i:i + 3, j:j + 3]))
+                output[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)  
+        
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        output = cv2.filter2D(output, -1, kernel)
+    elif flag == 2:
+        # create prewitt mask
+        Gx = np.array([[1.0, 0.0, -1.0], [1.0, 0.0, -1.0], [1.0, 0.0, -1.0]])
+        Gy = np.array([[1.0, 1.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -1.0, -1.0]])
+        [rows, columns] = np.shape(img)
+        output = np.zeros(shape=(rows, columns)) 
+
+        # Compute output in x and y directions
+        for i in range(rows - 2):
+            for j in range(columns - 2):
+                gx = np.sum(np.multiply(Gx, img[i:i + 3, j:j + 3]))
+                gy = np.sum(np.multiply(Gy, img[i:i + 3, j:j + 3]))
+                output[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)
+        
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        output = cv2.filter2D(output, -1, kernel)
+        output =otsu_t(output)
+    elif flag == 3:
+        # use canny edge detector
+        output = cv2.Canny(img, 40, 70)
+    return output
+
+# Pixel Division
+def improvement2(img):
+    # image
+    output1 = cv2.medianBlur(img,3)
+    # background mask
+    output2 = cv2.medianBlur(img,51)
+
+    # divide the 2 blur images to remove the background. Afterwards, normalize pixel intensity.
     output = np.ma.divide(output1, output2).data
-    output = np.uint8(255*output/output.max())
+    output = np.uint8(255 * output / output.max())
 
     # use simple 2D filter to sharpen the image
     kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
     output = cv2.filter2D(output, -1, kernel)
     return output
 
-
-def improvement2 (img):
-    Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
-    Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
-    [rows, columns] = np.shape(img)  # we need to know the shape of the input grayscale image
-    output = np.zeros(shape=(rows, columns))  # initialization of the output image array (all elements are 0)
-
-    # Now we "sweep" the image in both x and y directions and compute the output
-    for i in range(rows - 2):
-        for j in range(columns - 2):
-            gx = np.sum(np.multiply(Gx, img[i:i + 3, j:j + 3]))  # x direction
-            gy = np.sum(np.multiply(Gy, img[i:i + 3, j:j + 3]))  # y direction
-            output[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)  # calculate the "hypotenuse"
+# adaptive thresholding
+def improvement3(img, threshold = 25):
+    # Original image size
+    orignrows, origncols = img.shape
     
-    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    output = cv2.filter2D(output, -1, kernel)
+    # region size
+    width = int(np.floor(orignrows/16) + 1)
+    height = int(np.floor(origncols/16) + 1)
+    
+    # border padding
+    Mextend = round(width / 2) - 1
+    Nextend = round(height / 2) - 1
+    
+    # image padding
+    aux = cv2.copyMakeBorder(img, top=Mextend, bottom=Mextend, left=Nextend,right=Nextend, borderType=cv2.BORDER_REFLECT)
+    
+    region = np.zeros((width,height),np.int32)
+    
+    # calculate image integral
+    imageIntegral = cv2.integral(aux, region, -1)
+    
+    # get integral image size
+    nrows, ncols = imageIntegral.shape
+    
+    result = np.zeros((orignrows, origncols))
+    
+    # calculate cumulative pixels
+    for i in range(nrows - width):
+        for j in range(ncols - height):
+            result[i, j] = imageIntegral[i + width, j + height] - imageIntegral[i, j + height]+ imageIntegral[i, j] - imageIntegral[i + width, j]
+     
+    binary = np.ones((orignrows, origncols), dtype=np.bool)
+    img = (img).astype('float64') * width * height
+    
+    # image binarization
+    binary[img <= result * (100.0 - threshold) / 100.0] = False
+    output = (255 * binary).astype(np.uint8)
     return output
 
-def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
-    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-    sharpened = float(amount + 1) * image - float(amount) * blurred
-    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
-    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
-    sharpened = sharpened.round().astype(np.uint8)
-    if threshold > 0:
-        low_contrast_mask = np.absolute(image - blurred) < threshold
-        np.copyto(sharpened, image, where=low_contrast_mask)
-    return sharpened
-
-# for sanity check, cross-checking output with above function
-def otsu_lib(img):
-    _, output = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    cv2.imwrite(f'{debug_dir}/debug_otsu_{args.imname}.jpg', output)
-
-#### CV2 preprocessing functions
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
- 
-#thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-#dilation
-def dilate(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
-    
-#erosion
-def erode(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
-
-#opening - erosion followed by dilation
-def opening(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-#canny edge detection
-def canny(image):
-    return cv2.Canny(image, 100, 200)
-
-#skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
-
-#template matching
-def match_template(image, template):
-    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-
 ## Tesseract function
+# takes in an image to be fed into the LSTM-based engine
 def translate(img):
     print('Translating image...')
     # Adding custom options
@@ -166,13 +163,14 @@ def translate(img):
     result = pytesseract.image_to_string(img, config=custom_config)
     return result
 
-
+# program entry point
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Teserract OCR program parser.')
     parser.add_argument('--imname', default='sample01')
-    parser.add_argument('--otsu', default=False)
-    parser.add_argument('--otsu_improved1', default=False)
-    parser.add_argument('--otsu_improved2', default=False)
+    parser.add_argument('--otsu', default=True)
+    parser.add_argument('--improvement1', default=True)
+    parser.add_argument('--improvement2', default=True)
+    parser.add_argument('--improvement3', default=True)
     args = parser.parse_args()
 
     print('Reading image...')
@@ -184,27 +182,54 @@ if __name__ == '__main__':
     if args.otsu:
         output = otsu_t(img)
         cv2.imwrite(f'./processed_{args.imname}.jpg', output)
-        otsu_lib(img) # sanity check with cv2 otsu thresholding to ensure that our implementation is correct
         ocr_result = translate(output)
 
         with open(f'./result_otsu_{args.imname}.txt', 'w') as tf:
             tf.write(ocr_result)
 
-    ## Improvement1: Equalized illumination
-    if args.otsu_improved1:
-        output = improvement1(img)
-        cv2.imwrite(f'./processed_improved1_{args.imname}.jpg', output)
+    ## Improvement1: Edge detectors
+    if args.improvement1:
+        # sobel mask
+        output = improvement1(img, 1)
+        cv2.imwrite(f'./processed_improved1_sobel_{args.imname}.jpg', output)
+        output = cv2.imread(f'./processed_improved1_sobel_{args.imname}.jpg', 0)
         ocr_result = translate(output)
 
-        with open(f'./result_otsu_improved1_{args.imname}.txt', 'w') as tf:
+        with open(f'./result_otsu_improved1_sobel_{args.imname}.txt', 'w') as tf:
             tf.write(ocr_result)
 
-    ## Improvement2: 
-    if args.otsu_improved2:
+        # prewitt mask
+        output = improvement1(img, 2)
+        cv2.imwrite(f'./processed_improved1_prewitt_{args.imname}.jpg', output)
+        output = cv2.imread(f'./processed_improved1_prewitt_{args.imname}.jpg', 0)
+        ocr_result = translate(output)
+
+        with open(f'./result_otsu_improved1_prewitt_{args.imname}.txt', 'w') as tf:
+            tf.write(ocr_result)
+
+        # canny
+        output = improvement1(img, 3)
+        cv2.imwrite(f'./processed_improved1_canny_{args.imname}.jpg', output)
+        output = cv2.imread(f'./processed_improved1_canny_{args.imname}.jpg', 0)
+        ocr_result = translate(output)
+
+        with open(f'./result_otsu_improved1_canny_{args.imname}.txt', 'w') as tf:
+            tf.write(ocr_result)
+
+    ## Improvement2: Pixel Division
+    if args.improvement2:
         output = improvement2(img)
         cv2.imwrite(f'./processed_improved2_{args.imname}.jpg', output)
-        output = cv2.imread(f'./processed_improved2_{args.imname}.jpg', 0)
         ocr_result = translate(output)
 
         with open(f'./result_otsu_improved2_{args.imname}.txt', 'w') as tf:
+            tf.write(ocr_result)
+    
+    ## Improvement3: Adaptive thresholding
+    if args.improvement3:
+        output = improvement3(img)
+        cv2.imwrite(f'./processed_improved3_{args.imname}.jpg', output)
+        ocr_result = translate(output)
+
+        with open(f'./result_improved3_{args.imname}.txt', 'w') as tf:
             tf.write(ocr_result)
